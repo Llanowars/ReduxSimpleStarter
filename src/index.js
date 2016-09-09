073fd4cd5f110dd8ce42197ca390f155c9ec8c82
Julien Da Silva <julien.da.silva@hotmail.com>
2016-09-09 18:35:08 +0200

    end of the week

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'; 
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDmb6Al9P1LrVQGctwbOzGmy7SS4emh2YI';
 
 // Creer un composant qui rend du HTML
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfoards'}, (videos) => {
+      this.setState({ videos: videos });
+      // this.setState({ videos })
+      // Lorsque clé et valeurs sont les mêmes mots on peut simplifier la syntaxe comme çi dessus
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Rendre ce composant dans le DOM 
