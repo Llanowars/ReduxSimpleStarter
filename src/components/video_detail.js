905155ad4cc6fbce73c80951a860254bb846cd20
Julien Da Silva <julien.da.silva@hotmail.com>
2016-09-15 11:58:17 +0200

    checkpoint

---
+++ src/components/video_detail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const VideoDetail = ({video}) => {
+  const VideoID = video.id.videoId;
+  const Url = `https://www.youtube.com/embed/${VideoID}`;
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src='{Url}'></iframe>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
