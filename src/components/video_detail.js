cc69dc71cd760a1c0d186ab15488e6bad9a6afec
Julien Da Silva <julien.da.silva@hotmail.com>
2017-10-06 10:39:03 +0200

    checkpoint

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -8,9 +8,9 @@ const VideoDetail = ({video}) => {
   const url = `https://www.youtube.com/embed/${videoID}`;
   
   return (
-    <div className="video-detail col-md-8">
-      <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" src={url}></iframe>
+    <div className="">
+      <div className="">
+        <iframe className="" src={url}></iframe>
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>
